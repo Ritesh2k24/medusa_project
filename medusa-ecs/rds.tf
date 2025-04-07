c56aabfdced5c019f7b1cd59c16685ab911bf420
Ritesh2k24 <162950856+Ritesh2k24@users.noreply.github.com>
2025-04-07 12:59:25 +0530

    Delete medusa-ecs directory

--- medusa-ecs/rds.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_db_instance" "medusa_db" {
-  allocated_storage    = 20
-  engine              = "postgres"
-  instance_class      = "db.t3.micro"
-  username           = var.db_username
-  password           = var.db_password
-  skip_final_snapshot = true
-}
